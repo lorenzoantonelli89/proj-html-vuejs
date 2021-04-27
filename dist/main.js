896cea676373fe39aa87067bfda09d7efeae4943
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-04-27 21:23:15 +0200

    section 5 complete

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,18 @@ function initVue() {
     data: {
       'logoHeader': 'img/avada-bakery-logo.png',
       'linksHeader': ['HOME', 'SHOP', 'ABOUT', 'GALLERY', 'LOCATIONS', 'JOURNAL', 'CONTACT', 'MY ACCOUNT', '<i class="fas fa-shopping-cart"></i>'],
-      'scrollOn': false
+      'scrollOn': false,
+      'locations': [{
+        img: 'img/new-york-bk-800x530.jpg',
+        city: 'New York',
+        phone: '1.800.458.556',
+        openingTime: '9:00 AM - 6:00 PM'
+      }, {
+        img: 'img/london-bk-800x530.jpg',
+        city: 'London',
+        phone: '1.800.458.556',
+        openingTime: '9:00 AM - 6:00 PM'
+      }]
     },
     mounted: function mounted() {
       document.addEventListener('scroll', this.scrollUp);
--- src/main.js
+++ src/main.js
@@ -16,6 +16,20 @@ function initVue() {
                 '<i class="fas fa-shopping-cart"></i>',
             ],
             'scrollOn': false,
+            'locations': [
+                {
+                    img: 'img/new-york-bk-800x530.jpg',
+                    city: 'New York',
+                    phone: '1.800.458.556',
+                    openingTime: '9:00 AM - 6:00 PM',
+                },
+                {
+                    img: 'img/london-bk-800x530.jpg',
+                    city: 'London',
+                    phone: '1.800.458.556',
+                    openingTime: '9:00 AM - 6:00 PM',
+                }
+            ]
 
         },
         mounted() {
