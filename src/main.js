3fd0eff485948c09a2c3356833191b4bab4def4d
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-04-28 13:12:47 +0200

    update carousel sect 1 e 3 finiti

--- src/main.js
+++ src/main.js
@@ -17,6 +17,48 @@ function initVue() {
             ],
             'activeLinkHeader': 0,
             'scrollOn': false,
+            'carouselOne': [
+                {
+                    img: 'img/choco-chip-cookies-400x510.jpg',
+                    name: 'Choco Chip Cookies',
+                    category1: 'Cookies',
+                    category2: 'Pastries',
+                    currency: '$',
+                    value1: '18.00',
+                    value2: '36.00',
+                    visibility: true,
+                },
+                {
+                    img: 'img/strawberry-jam-cookies-400x510.jpg',
+                    name: 'Strawberry Jam Cookies',
+                    category1: 'Cookies',
+                    category2: 'Pastries',
+                    currency: '$',
+                    value1: '36.00',
+                    value2: '60.00',
+                    visibility: true,
+                },
+                {
+                    img: 'img/cookies-with-ice-cream-400x510.jpg',
+                    name: 'Cookies with ice cream',
+                    category1: 'Cookies',
+                    category2: 'Pastries',
+                    currency: '$',
+                    value1: '18.00',
+                    value2: '34.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/home-bread-400x510.jpg',
+                    name: 'Homemade Bread',
+                    category1: 'Bread',
+                    category2: 'Pastries',
+                    currency: '$',
+                    value1: '26.00',
+                    value2: '68.00',
+                    visibility: false,
+                },
+            ],
             'locations': [
                 {
                     img: 'img/new-york-bk-800x530.jpg',
@@ -30,6 +72,80 @@ function initVue() {
                     phone: '1.800.458.556',
                     openingTime: '9:00 AM - 6:00 PM',
                 }
+            ],
+            'carouselTwo': [
+                {
+                    img: 'img/choco-chip-cookies-200x255.jpg',
+                    name: 'Choco Chip Cookies',
+                    value: '$18.00 - $32.00',
+                    visibility: true,
+                },
+                {
+                    img: 'img/strawberry-jam-cookies-200x255.jpg',
+                    name: 'Strawberry Jam Cookies',
+                    value: '$36.00 - $60.00',
+                    visibility: true,
+                },
+                {
+                    img: 'img/strawberry-donut-200x255.jpg',
+                    name: 'Strawberry Donut',
+                    value: '$24.00 - $48.00',
+                    visibility: true,
+                },
+                {
+                    img: 'img/perfect-macarons-200x255.jpg',
+                    name: 'Perfect Macarons',
+                    value: '$32.00 - $56.00',
+                    visibility: true,
+                },
+                {
+                    img: 'img/premium-bread-200x255.jpg',
+                    name: 'Premium Bread',
+                    value: '$32.00 - $68.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/cherry-cake-200x255.jpg',
+                    name: 'Cake with Cherry Topping',
+                    value: '$99.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/cookies-with-ice-cream-200x255.jpg',
+                    name: 'Cookies with ice cream',
+                    value: '$18.00 - $34.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/blackberry-stuffed-bread-200x255.jpg',
+                    name: 'Blackberry Stuffed Bread',
+                    value: '$22.00 - $46.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/glazed-pancake-with-lemon-200x255.jpg',
+                    name: 'Glazed Bread with Fruits',
+                    value: '$24.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/small-cupcake-200x255.jpg',
+                    name: 'Chocolate Cupcake',
+                    value: '$26.00 - $68.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/home-bread-200x255.jpg',
+                    name: 'Homemade Bread',
+                    value: '$26.00 - $68.00',
+                    visibility: false,
+                },
+                {
+                    img: 'img/premium-bread-200x255.jpg',
+                    name: 'Premium Bread',
+                    value: '$32.00 - $68.00',
+                    visibility: false,
+                },
             ],
             'linksFooter': [
                 'Shop',
@@ -47,7 +163,7 @@ function initVue() {
                 'fa-twitter',
                 'fa-facebook-f',
                 'fa-pinterest-p',
-            ]
+            ],
 
         },
         mounted() {
@@ -62,10 +178,55 @@ function initVue() {
             },
             clickLink: function (index) {
                 this.activeLinkHeader = index;
+            },
+            sliderOne: function () {
+                if (this.carouselOne[0].visibility == true && this.carouselOne[1].visibility == true){
+                    this.carouselOne[0].visibility = false; 
+                    this.carouselOne[1].visibility = false;
+                    this.carouselOne[2].visibility = true;
+                    this.carouselOne[3].visibility = true;
+                    
+                }else if (this.carouselOne[2].visibility == true && this.carouselOne[3].visibility == true){
+                    this.carouselOne[2].visibility = false;
+                    this.carouselOne[3].visibility = false;
+                    this.carouselOne[0].visibility = true;
+                    this.carouselOne[1].visibility = true;
+                    
+                }
+            },
+            sliderTwo: function () {
+                if (this.carouselTwo[0].visibility == true && this.carouselTwo[1].visibility == true && this.carouselTwo[2].visibility == true && this.carouselTwo[3].visibility == true) {
+                    this.carouselTwo[0].visibility = false;
+                    this.carouselTwo[1].visibility = false;
+                    this.carouselTwo[2].visibility = false;
+                    this.carouselTwo[3].visibility = false;
+                    this.carouselTwo[4].visibility = true;
+                    this.carouselTwo[5].visibility = true;
+                    this.carouselTwo[6].visibility = true;
+                    this.carouselTwo[7].visibility = true;
+                } else if (this.carouselTwo[4].visibility == true && this.carouselTwo[5].visibility == true && this.carouselTwo[6].visibility == true && this.carouselTwo[7].visibility == true) {
+                    this.carouselTwo[4].visibility = false;
+                    this.carouselTwo[5].visibility = false;
+                    this.carouselTwo[6].visibility = false;
+                    this.carouselTwo[7].visibility = false;
+                    this.carouselTwo[8].visibility = true;
+                    this.carouselTwo[9].visibility = true;
+                    this.carouselTwo[10].visibility = true;
+                    this.carouselTwo[11].visibility = true;
+                } else if (this.carouselTwo[8].visibility == true && this.carouselTwo[9].visibility == true && this.carouselTwo[10].visibility == true && this.carouselTwo[11].visibility == true) {
+                    this.carouselTwo[8].visibility = false;
+                    this.carouselTwo[9].visibility = false;
+                    this.carouselTwo[10].visibility = false;
+                    this.carouselTwo[11].visibility = false;
+                    this.carouselTwo[0].visibility = true;
+                    this.carouselTwo[1].visibility = true;
+                    this.carouselTwo[2].visibility = true;
+                    this.carouselTwo[3].visibility = true;
+                }
             }
         },
         computed: {
-
+            
         },
     });
 }
